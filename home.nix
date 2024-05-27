a5825488c3a18717f3c2f3a19161d365980eada6
gapuchi <arjun.adhia@gmail.com>
2024-05-27 17:57:13 -0400

    NixOS Config Init

---
+++ home.nix
@@ -0,0 +1,44 @@
+{ pkgs, ... }: {
+
+  home.username = "gapuchi";
+  home.homeDirectory = "/home/gapuchi";
+
+  home.packages = with pkgs; [
+    fzf
+    git
+    google-chrome
+    nodejs_18
+    rustup
+    tmux
+    vscode
+  ];
+
+  home.sessionPath = [
+    "$HOME/.cargo/bin"
+  ];
+
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" "tmux" "fzf" ];
+      theme = "robbyrussell";
+    };
+  };
+
+  # The state version is required and should stay at the version you
+  # originally installed.
+  home.stateVersion = "23.05";
+
+  # Let home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
