f953ed36a289693115ef8e2cbde9fb8aa05c090a
gapuchi <arjun.adhia@gmail.com>
2024-05-30 03:01:37 -0400

    Move Laptop Config To Common

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }: 
+    let
+      hosts = [
+        "gapuchi-desktop"
+        "gapuchi-laptop"
+      ];
+
+      forAllHosts = nixpkgs.lib.genAttrs hosts;
+   in {
+    nixosConfigurations = forAllHosts (host: nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./${host}/configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.gapuchi = import ./${host}/home.nix;
+        }
+      ];
+    });
+  };
+}
