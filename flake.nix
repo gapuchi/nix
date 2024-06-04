5a78637731640ff0d69a8df0fd58cad9ece18027
gapuchi <arjun.adhia@gmail.com>
2024-06-03 23:26:37 -0400

    Add Desktop Module

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: 
     let
-      hosts = {
+      hostConfigs = {
         "gapuchi-desktop" = {
           stateVersion = "23.05";
         };
@@ -19,18 +19,17 @@
         };
       };
 
-      forAllHosts = nixpkgs.lib.genAttrs (builtins.attrNames hosts);
+      forAllHosts = nixpkgs.lib.genAttrs (builtins.attrNames hostConfigs);
    in {
     nixosConfigurations = forAllHosts (hostName: nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./machines/${hostName}/configuration.nix
-        home-manager.nixosModules.home-manager
-        {
+        home-manager.nixosModules.home-manager {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.gapuchi = import ./common/home.nix;
-          home-manager.extraSpecialArgs = hosts.${hostName};
+          home-manager.extraSpecialArgs = hostConfigs.${hostName};
         }
       ];
     });
