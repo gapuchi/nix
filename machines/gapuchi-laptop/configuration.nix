3086e24e4ef701a771a92f71879d8e87f31f12b3
gapuchi <arjun.adhia@gmail.com>
2024-06-10 11:56:16 -0400

    Disable Touchpad While Typing

--- machines/gapuchi-laptop/configuration.nix
+++ machines/gapuchi-laptop/configuration.nix
@@ -79,6 +79,7 @@
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
+  services.libinput.touchpad.disableWhileTyping = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.gapuchi = {
