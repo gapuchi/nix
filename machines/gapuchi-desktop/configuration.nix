c5b45f56c9d6d1a9215fdccad3b104f059aa7aae
gapuchi <arjun.adhia@gmail.com>
2025-03-02 23:10:06 -0500

    Update Flake

--- machines/gapuchi-desktop/configuration.nix
+++ machines/gapuchi-desktop/configuration.nix
@@ -91,7 +91,7 @@
   };
 
   nix = {
-    package = pkgs.nixFlakes;
+    package = pkgs.nixVersions.stable;
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
--- machines/gapuchi-laptop/configuration.nix
+++ machines/gapuchi-laptop/configuration.nix
@@ -88,7 +88,7 @@
   };
 
   nix = {
-    package = pkgs.nixFlakes;
+    package = pkgs.nixVersions.stable;
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
