b462a4a518c67fd93762c4d6180dbfe6889aaa40
gapuchi <arjun.adhia@gmail.com>
2025-10-21 00:33:36 -0400

    Config

--- flake.nix
+++ flake.nix
@@ -9,45 +9,40 @@
     };
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: 
-    let
-      hostConfigs = {
-        "gapuchi-desktop" = {
-          stateVersion = "23.05";
-          includeGuiPkgs = true;
-          homeDirectory = "/home/gapuchi";
-        };
+  outputs = { nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      "gapuchi-desktop" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./machines/gapuchi-desktop/configuration.nix
+          home-manager.nixosModules.home-manager.home-manager {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.gapuchi = import ./common/home.nix;
+            extraSpecialArgs = {
+              stateVersion = "23.05";
+              includeGuiPkgs = true;
+              homeDirectory = "/home/gapuchi";
+            };
+          }
+        ];
       };
-
-      forAllHosts = nixpkgs.lib.genAttrs (builtins.attrNames hostConfigs);
-   in {
-    nixosConfigurations = forAllHosts (hostName: nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./machines/${hostName}/configuration.nix
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.gapuchi = import ./common/home.nix;
-          home-manager.extraSpecialArgs = hostConfigs.${hostName};
-        }
-      ];
-    });
+    };
 
     homeConfigurations = {
       "gapuchi@gapuchi-air" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-        extraSpecialArgs = {
-          stateVersion = "25.05";
-          includeGuiPkgs = false;
-          homeDirectory = "/Users/gapuchi";
-        };
         modules = [ 
           ./common/home.nix
           {
             nixpkgs.config.allowUnfree = true;
           }
         ];
+        extraSpecialArgs = {
+          stateVersion = "25.05";
+          includeGuiPkgs = false;
+          homeDirectory = "/Users/gapuchi";
+        };
       };
     };
   };
