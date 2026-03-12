a0175231b34cd0fad0eef905ccd9cb9eed652e8c
gapuchi <arjun.adhia@gmail.com>
2026-03-12 18:18:52 -0400

    Have Home Manager Follow Stable Release

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
