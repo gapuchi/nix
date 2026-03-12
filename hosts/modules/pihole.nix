369a4cf8d4b27fb61d64b8d016e6ddd71d3f249b
gapuchi <arjun.adhia@gmail.com>
2026-03-12 17:07:24 -0400

    Remove Unnecessary Pihole Config

--- hosts/modules/pihole.nix
+++ hosts/modules/pihole.nix
@@ -1,18 +1,6 @@
 { pkgs, ... }:
 {
 
-  networking = {
-    interfaces.enp3s0.ipv4.addresses = [
-      {
-        address = "192.168.1.2";
-        prefixLength = 24;
-      }
-    ];
-
-    defaultGateway = "192.168.1.1";
-    nameservers = [ "127.0.0.1" ];
-  };
-
   services.pihole-ftl = {
     enable = true;
 
