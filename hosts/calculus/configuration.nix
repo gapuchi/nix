a7dd9799e80c1598d01fb50580d2f460ebccd59c
gapuchi <arjun.adhia@gmail.com>
2026-03-12 18:11:47 -0400

    Disable Password SSH

--- hosts/calculus/configuration.nix
+++ hosts/calculus/configuration.nix
@@ -109,7 +109,12 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    # require public key authentication for better security
+    settings.PasswordAuthentication = false;
+    settings.KbdInteractiveAuthentication = false;
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
