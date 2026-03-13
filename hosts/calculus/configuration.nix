763480efb24466f71a84b6d86fd7e13f27a06800
gapuchi <arjun.adhia@gmail.com>
2026-03-13 14:05:19 -0400

    Support ghostty term info

--- hosts/calculus/configuration.nix
+++ hosts/calculus/configuration.nix
@@ -100,6 +100,7 @@
     git
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+    ghostty.terminfo
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
