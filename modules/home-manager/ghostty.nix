2164920e5ccc45c651ba55db8530444ed3ed9223
gapuchi <arjun.adhia@gmail.com>
2026-07-15 12:53:39 -0400

    Add Quick Terminal Config

--- modules/home-manager/ghostty.nix
+++ modules/home-manager/ghostty.nix
@@ -6,7 +6,15 @@
       programs.ghostty = {
         enable = true;
         enableZshIntegration = true;
-        settings.theme = "Vercel";
+        settings = {
+          theme = "Vercel";
+          keybind = [
+            "global:ctrl+grave_accent=toggle_quick_terminal"
+          ];
+          quick-terminal-position = "center";
+          quick-terminal-animation-duration = 0.1;
+          quick-terminal-size = "80%,70%";
+        };
       };
     };
 }
